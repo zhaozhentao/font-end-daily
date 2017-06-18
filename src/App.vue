c1a2ab6f26b70e255dc98657637821474daa39f5
zhaozhentao <344696734@qq.com>
2017-06-18 16:50:57 +0800

    插入logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <daily-nav></daily-nav>
     <router-view></router-view>
   </div>
 </template>
@@ -20,5 +21,28 @@
 </script>
 
 <style>
+  html::after {
+    background-attachment: fixed;
+    content: '';
+    will-change: transform;
+    z-index: -1;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    position: fixed;
+    background: url(./assets/andy.jpeg);
+    background-size: cover;
+  }
 
+  html, body {
+    height: 100%;
+    font-size: 14px;
+    color: #525252;
+    font-family: NotoSansHans-Regular,AvenirNext-Regular,arial,Hiragino Sans GB,"Microsoft Yahei","Hiragino Sans GB","WenQuanYi Micro Hei",sans-serif;
+  }
+
+  html, body {
+    background: transparent;
+  }
 </style>
