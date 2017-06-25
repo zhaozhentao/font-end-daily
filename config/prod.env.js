865ea4bc331c2401f5576a103c93ea428e30e92f
zhaozhentao <344696734@qq.com>
2017-06-25 22:07:58 +0800

    nginx反向代理测试

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  // baseUrl: '"http://47.92.113.116:8080"'
-  baseUrl: '"http://localhost:8080"'
+  baseUrl: '"http://47.92.113.116:8081"'
+  // baseUrl: '"http://localhost:8080"'
 }
