43c02557f7c29fb17192f7ebc0ed0f1faaf78bbc
zhaozhentao <344696734@qq.com>
2017-06-29 00:02:24 +0800

    editor修改

--- src/views/blog/blog.js
+++ src/views/blog/blog.js
@@ -1,3 +1,5 @@
+import SimpleMDE from '../../../public/assets/js/editor'
+
 const state = {
   count: 0
 }
@@ -6,6 +8,41 @@ const mutations = {
 }
 
 const actions = {
+  initEditor (store, data) {
+    var simplemde = new SimpleMDE({
+      spellChecker: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+        unique_id: 1
+      },
+      forceSync: true
+    })
+
+    window.inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
+      uploadUrl: '',
+      extraParams: {
+        '_token': ''
+      },
+      onFileUploadResponse: function (xhr) {
+        var result = JSON.parse(xhr.responseText)
+        var filename = result[this.settings.jsonFieldName]
+
+        if (result && filename) {
+          var newValue
+          if (typeof this.settings.urlText === 'function') {
+            newValue = this.settings.urlText.call(this, filename, result)
+          } else {
+            newValue = this.settings.urlText.replace(this.filenameTag, filename)
+          }
+          var text = this.editor.getValue().replace(this.lastValue, newValue)
+          this.editor.setValue(text)
+          this.settings.onFileUploaded.call(this, filename)
+        }
+        return false
+      }
+    })
+  }
 }
 
 const getters = {
--- src/views/blog/blog.vue
+++ src/views/blog/blog.vue
@@ -20,46 +20,11 @@
 </template>
 
 <script>
-  import SimpleMDE from '../../../public/assets/js/editor'
-
   export default {
     mounted () {
-      var simplemde = new SimpleMDE({
-        spellChecker: false,
-        autosave: {
-          enabled: true,
-          delay: 1000,
-          unique_id: 1
-        },
-        forceSync: true
-      })
-
-      window.inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
-        uploadUrl: '',
-        extraParams: {
-          '_token': ''
-        },
-        onFileUploadResponse: function (xhr) {
-          var result = JSON.parse(xhr.responseText)
-          var filename = result[this.settings.jsonFieldName]
-
-          if (result && filename) {
-            var newValue
-            if (typeof this.settings.urlText === 'function') {
-              newValue = this.settings.urlText.call(this, filename, result)
-            } else {
-              newValue = this.settings.urlText.replace(this.filenameTag, filename)
-            }
-            var text = this.editor.getValue().replace(this.lastValue, newValue)
-            this.editor.setValue(text)
-            this.settings.onFileUploaded.call(this, filename)
-          }
-          return false
-        }
-      })
+      this.$store.dispatch('initEditor')
     }
   }
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
