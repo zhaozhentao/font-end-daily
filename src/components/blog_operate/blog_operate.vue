d9072b87eb58e0b5250a93bb1e6d8ede4d2b021a
zhaozhentao <344696734@qq.com>
2017-07-04 20:37:36 +0800

    删除没用的附言等功能

--- src/components/blog_operate/blog_operate.vue
+++ src/components/blog_operate/blog_operate.vue
@@ -6,20 +6,6 @@
       </div>
 
       <div class="pull-right actions">
-        <a href="javascript:void(0);" class="admin popover-with-html" data-content="推荐主题，加精的帖子会出现在首页">
-          <i class="fa fa-trophy"></i>
-        </a>
-
-        <a href="javascript:void(0);"
-           class="admin popover-with-html" data-content="帖子置顶，会在列表页置顶">
-          <i class="fa fa-thumb-tack"></i>
-        </a>
-
-        <a @click="deleteBlog()" href="javascript:void(0);" class="admin popover-with-html "
-           data-content="沉贴，帖子将会被降低排序优先级">
-          <i class="fa fa-anchor"></i>
-        </a>
-
         <a @click="deleteBlog()" href="javascript:void(0);" class="admin">
           <i class="fa fa-trash-o"></i>
         </a>
@@ -28,50 +14,8 @@
           :to="{ name: 'blog_edit', params: { id: $route.params.id }}" class="admin">
           <i class="fa fa-pencil-square-o"></i>
         </router-link>
-
-        <a href="javascript:void(0);" class="admin popover-with-html" data-toggle="modal"
-           data-target="#exampleModal" data-content="帖子附言，添加附言后所有参与讨论的用户都能收到消息提醒，包括点赞和评论的用户">
-          <i class="fa fa-plus"></i>
-        </a>
       </div>
       <div class="clearfix"></div>
-    </div>
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="" aria-labelledby="exampleModalLabel">
-      <div class="modal-dialog">
-        <div class="modal-content">
-
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-              aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="exampleModalLabel">添加附言</h4>
-          </div>
-
-          <form method="POST" action="" accept-charset="UTF-8">
-            <div class="modal-body">
-
-              <div class="alert alert-warning">
-                附加内容, 使用此功能的话, 会给所有参加过讨论的人发送提醒.
-              </div>
-
-              <div class="form-group">
-                <textarea
-                  class="form-control" style="min-height:20px"
-                  placeholder="使用markdown" name="content" cols="50"
-                  rows="10"></textarea>
-              </div>
-
-            </div>
-
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-              <button type="submit" class="btn btn-primary">提交</button>
-            </div>
-
-          </form>
-
-        </div>
-      </div>
     </div>
   </div>
 </template>
